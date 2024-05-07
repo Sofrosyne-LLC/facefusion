987d48e739c47191898145d46f6ff076f34163b8
henryruhs <info@henryruhs.com>
2024-05-07 18:14:42 +0200

    Add final page to installer, disable version check for Gradio

--- facefusion/uis/core.py
+++ facefusion/uis/core.py
@@ -10,6 +10,7 @@ from facefusion import metadata, logger, wording
 from facefusion.uis.typing import Component, ComponentName
 from facefusion.filesystem import resolve_relative_path
 
+gradio.analytics.version_check = None
 gradio.processing_utils.encode_array_to_base64 = overrides.encode_array_to_base64
 gradio.processing_utils.encode_pil_to_base64 = overrides.encode_pil_to_base64
 
